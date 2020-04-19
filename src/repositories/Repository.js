5497f9401f5be0e70a6fbefb71ecc453537e1181
lethanhtupk <lethanhtu1551998@gmail.com>
2020-04-19 20:06:24 +0700

    updated

--- src/repositories/Repository.js
+++ src/repositories/Repository.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseDomain = "http://127.0.0.1:8000"
+const baseDomain = "https://tult-photos-gallery.herokuapp.com"
 const baseURL = `${baseDomain}/api/v1`;
 
 export default axios.create({
